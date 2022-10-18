8fc867fa3c8c1821683223335884277cce954601
ramseymostafa3 <105738447+ramseymostafa3@users.noreply.github.com>
2022-10-18 19:01:59 +0500

    New parameter in optimisation tab (#3)
    * feedback done on 3 tabs (#3)
    
    * addition in optimisation tab in general page

--- src/components/general/OptimisationTable/index.js
+++ src/components/general/OptimisationTable/index.js
@@ -29,7 +29,6 @@ function OptimisationTable() {
                     </tr>
                     <tr>
                         <td>Fasts</td>
-                        
                         <td><NumberFormat className='w100' name="" defaultValue='45' displayType={'input'}  /></td>
                         <td><NumberFormat className='w100' name="" defaultValue='60' displayType={'input'}  /></td>
                         <td><NumberFormat className='w100' name="" defaultValue='1' displayType={'input'}  /></td>
@@ -38,6 +37,72 @@ function OptimisationTable() {
                         <td>Slows</td>
                         <td><NumberFormat className='w100' name="" defaultValue='189' displayType={'input'}  /></td>
                         <td><NumberFormat className='w100' name="" defaultValue='210' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='1' displayType={'input'}  /></td>
+                    </tr>
+                    <tr>
+                        <td>K</td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                    </tr>
+                    <tr>
+                        <td>Gain</td>
+                        <td><NumberFielder defaultvalue='1.5' /></td>
+                        <td><NumberFielder defaultvalue='1.5' /></td>
+                        <td><NumberFielder defaultvalue='1.5' /></td>
+                    </tr>
+                    <tr>
+                        <td>ROC</td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                    </tr>
+                    <tr>
+                        <td>ROCW</td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                    </tr>
+                    <tr>
+                        <td>RSIDF</td>
+                        <td><NumberFielder defaultvalue='0' /></td>
+                        <td><NumberFielder defaultvalue='0' /></td>
+                        <td><NumberFielder defaultvalue='0' /></td>
+                    </tr>
+                    <tr>
+                        <td>RSIEMA</td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                        <td><NumberFielder defaultvalue='1' /></td>
+                    </tr>
+                    <tr>
+                        <td>RSIP</td>
+                        <td><NumberFielder defaultvalue='6' /></td>
+                        <td><NumberFielder defaultvalue='6' /></td>
+                        <td><NumberFielder defaultvalue='6' /></td>
+                    </tr>
+                    <tr>
+                        <td>RSILBs</td>
+                        <td><NumberFormat className='w100' name="" defaultValue='8' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='15' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='1' displayType={'input'}  /></td>
+                    </tr>
+                    <tr>
+                        <td>RSIHBs</td>
+                        <td><NumberFormat className='w100' name="" defaultValue='18' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='28' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='1' displayType={'input'}  /></td>
+                    </tr>
+                    <tr>
+                        <td>RSILSs</td>
+                        <td><NumberFormat className='w100' name="" defaultValue='65' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='75' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='1' displayType={'input'}  /></td>
+                    </tr>
+                    <tr>
+                        <td>RSIHSs</td>
+                        <td><NumberFormat className='w100' name="" defaultValue='84' displayType={'input'}  /></td>
+                        <td><NumberFormat className='w100' name="" defaultValue='86' displayType={'input'}  /></td>
                         <td><NumberFormat className='w100' name="" defaultValue='1' displayType={'input'}  /></td>
                     </tr>
                 </tbody>
